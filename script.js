19b78d96e7c8a6d65edbb7ef4a8c183e2b75bf85
Bhuvana123BG <gbhuvana938@gmail.com>
2026-01-28 16:58:15 +0530

    js

--- script.js
+++ script.js
@@ -149,5 +149,5 @@ let messages = [
   // Init
   renderMessageList();
   showRandom();
-  setInterval(showRandom,3000);
+  setInterval(showRandom,1000);
   document.getElementById("addBtn").addEventListener("click", addMessage);
